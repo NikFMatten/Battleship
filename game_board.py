2174aba3aa3e07e645a8feef2a4dcc96908a5346
Nikolaus Matten <nfmatten@gmail.com>
2022-09-14 11:58:24 -0600

    test commit

---
+++ game_board.py
@@ -0,0 +1,14 @@
+
+
+class GameBoard:
+    def __init__(self):
+        self.game_board = self.create_board()
+
+    def create_board(self):
+        
+
+
+
+
+
+        #\u25A2 
---
+++ main.py
@@ -0,0 +1,3 @@
+from game_board import GameBoard
+
+new_game = GameBoard()
